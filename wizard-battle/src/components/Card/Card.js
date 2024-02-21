d143856be71392bf14963d34932bbf0ea238361e
Menshikova Nastasya <menshikova.naya@mail.ru>
2024-02-21 12:59:51 +0500

    ручной выбор

--- wizard-battle/src/components/Card/Card.js
+++ wizard-battle/src/components/Card/Card.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import styles from './Card.module.css';
 
-export default function Card({ name }) {
+export default function Card({ name, toggleSelectionOpponent, colorPlace }) {
+  const location = useLocation();
+  const button = location.pathname === '/manual-selection';
   return (
           <>
             <div className={styles.card}>
                <h2 className={styles.card__name}>{name}</h2>
                <h2 className={styles.card__health}></h2>
                <h2 className={styles.card__mana}></h2>
+               {button && <button
+               onClick={toggleSelectionOpponent}
+               style={{ backgroundColor: colorPlace ? 'green' : 'red' }}
+               >Выбрать</button>}
             </div>
           </>
   );
--- wizard-battle/src/components/IndependentSelect/IndependentSelect.js
+++ wizard-battle/src/components/IndependentSelect/IndependentSelect.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './IndependentSelect.module.css';
+import Card from '../Card/Card';
+
+const wizzards = [
+  {
+    name: 'Harry',
+    lastName: 'Potter',
+    healthPoints: 100,
+    manaPoints: 100,
+    status: 'active',
+  },
+
+  {
+    name: 'Sirius',
+    lastName: 'Snake',
+    healthPoints: 100,
+    manaPoints: 1000,
+    status: 'active',
+  },
+
+  {
+    name: 'Hermiona',
+    lastName: 'Granger',
+    healthPoints: 100,
+    manaPoints: 1000,
+    status: 'active',
+  },
+];
 
 export default function IndependentSelect() {
+  const [isDisableButton, setIsDisableButton] = useState(false);
+  // const [selectFirstOpponent, setSelectFirstOpponent] = useState(false);
+  // const [selectSecondOpponent, setSelectSecondOpponent] = useState(false);
+  const [colorPlace, setColorPlace] = useState(false);
+  const [selectOpponent, setSelectOpponent] = useState(false);
+  const toggleSelectionOpponent = () => {
+    setSelectOpponent(!selectOpponent);
+    setColorPlace(!colorPlace);
+  };
+  const wizzardCards = wizzards.map((wizzard) => <Card
+  colorPlace={colorPlace}
+  toggleSelectionOpponent = {toggleSelectionOpponent}
+  key={wizzard.name}
+  name={wizzard.name}/>);
   return (
           <>
-            <section className={styles.header}>
-              <h1>Выбор вручную</h1>
+            <section className={styles.manual}>
+              <div className={styles.manual__container}>
+                {wizzardCards}
+              </div>
+              <button disabled={isDisableButton} className={styles.manual__button}>В бой</button>
             </section>
           </>
   );
